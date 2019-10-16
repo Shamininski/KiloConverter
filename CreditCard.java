6ef5f630f6c1a45c643cd5b1455fe797e6e3f10e
Noel Mbuya <mbuyanoel@gmail.com>
2019-10-16 22:59:29 +0300

    The initial code, just main method

---
+++ CreditCard.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+/** 
+    This program validates card numbers by use of Luhn Algorithm.
+*/
+
+
+public class CreditCard 
+{
+    public static void main (String[] args)
+    {
+        
+    }
+}
